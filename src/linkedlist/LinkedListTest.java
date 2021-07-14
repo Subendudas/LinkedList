610c2737a1aef2bc17ce41491b12ae4145995822
Subendu Das <31320094+Subendudas@users.noreply.github.com>
2021-07-14 15:07:50 +0530

    Ability to create Linked List by adding 30 and 56 to 70.

---
+++ src/linkedlist/LinkedListTest.java
@@ -0,0 +1,13 @@
+package linkedlist;
+public class LinkedListTest {
+	 public static void main(String[] args)
+	    {
+		   LinkedList linklist = new LinkedList();
+	        linklist.append(70);
+	        linklist.push(30);
+	        linklist.push(56);
+	        System.out.println("\nWelcome to LinkedList program:");
+	        linklist.printList();
+	    }
+
+}
